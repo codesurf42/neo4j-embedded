8e940856fe6a51418c4ca4c75a94ae014f8dad79
Adam Bartosik <abartosik@virtuslab.com>
2015-02-09 16:33:52 +0100

    time-stats

--- src/main/scala/com/ex1/App.scala
+++ src/main/scala/com/ex1/App.scala
@@ -14,26 +14,56 @@ object App {
     println("db shutdown")
   }
 
-  def createNodes(implicit graphDb: GraphDatabaseService): Unit = {
-    for (t <- 1 to 1000) {
-      val tx = graphDb.beginTx()
-      print(s"t: $t ")
-      try {
-        for (j <- 1 to 100) {
-          val firstNode = graphDb.createNode()
-          firstNode.setProperty("node", s"root $j")
+  case class Timings(time: Long, nodes: Int)
 
-          for (i <- 1 to 100) {
-            val subNode = graphDb.createNode()
-            subNode.setProperty("node", s"$j-$i")
-            firstNode.createRelationshipTo(subNode, DynamicRelationshipType.withName("state"))
+  def createNodes(implicit graphDb: GraphDatabaseService): Unit = {
+    var totalNodes = 0
+    val stats = collection.mutable.Map[Int, Timings]()
+    for (t <- 1 to 4) {
+      print(s"t: $t ")
+      for (howNested <- List(1, 3, 5, 7, 10, 20, 50)) {
+        var nodes = 0
+        val start = System.currentTimeMillis()
+
+        for (j <- 1 to 100) {
+          val tx = graphDb.beginTx()
+          try {
+            val firstNode = graphDb.createNode()
+            firstNode.setProperty("node", s"root $j")
+
+            for (i <- 1 to 100 * howNested) {
+              val subNode = graphDb.createNode()
+              subNode.setProperty("node", s"$j-$i")
+              firstNode.createRelationshipTo(subNode, DynamicRelationshipType.withName("state"))
+
+              nodes += 1
+            }
+            tx.success()
+          } finally {
+            tx.close()
           }
         }
-        tx.success()
-      } finally {
-        tx.close()
-      }
 
+        val offset = System.currentTimeMillis() - start
+        stats += (howNested -> stats.
+          get(howNested).
+          fold(Timings(offset, nodes))(v =>
+          Timings(v.time + offset, v.nodes + nodes)
+          ))
+        totalNodes += nodes
+
+      }
+    }
+
+    println()
+    println("All nodes: " + totalNodes)
+    stats.keys.toList.sorted.foreach { key =>
+      val times = stats.get(key).get
+      printf("Leafs: % 7d Time[sec]: % 8.3f Nodes: % 10d Nodes/sec: % 4d\n",
+        key * 100,
+        (times.time / 1000.0),
+        times.nodes,
+        (times.nodes / times.time))
     }
   }
 
